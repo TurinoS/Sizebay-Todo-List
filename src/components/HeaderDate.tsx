96c9e797091b25f5d7957d5fb1622ed7d67b8d6b
Paulo Turino <paulo.turino01@gmail.com>
2023-10-10 11:35:07 -0300

    feat: Dynamically display date in the header

--- src/components/HeaderDate.tsx
+++ src/components/HeaderDate.tsx
@@ -3,6 +3,8 @@ import styled from "styled-components";
 export const Header = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: baseline;
+  text-transform: capitalize;
   color: #666;
 
   & div {
@@ -11,7 +13,7 @@ export const Header = styled.div`
     align-items: center;
 
     & h1 {
-      font-size: 40px;
+      font-size: 50px;
       grid-row: 1 / 3;
       line-height: 40px;
     }
@@ -25,14 +27,21 @@ export const Header = styled.div`
 `
 
 export default function HeaderDate() {
-    return(
-        <Header>
-            <div>
-            <h1>07</h1>
-            <p>Jul</p>
-            <p>2021</p>
-            </div>
-            <h2>Quarta-feira</h2>
-        </Header>
+  const currentDate = new Date();
+
+  const day = currentDate.getDate();
+  const month = currentDate.toLocaleString("default", { month: "short" });
+  const year = currentDate.getFullYear();
+  const dayOfWeek = currentDate.toLocaleString("default", { weekday: "long" });
+
+  return (
+    <Header>
+      <div>
+        <h1>{day}</h1>
+        <p>{month}</p>
+        <p>{year}</p>
+      </div>
+      <h2>{dayOfWeek}</h2>
+    </Header>
     )
 }
